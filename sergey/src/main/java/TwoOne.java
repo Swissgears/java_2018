6da381073cc9a9e571a704941fb652a572192bb4
Swissgears <sergius.barannik@gmail.com>
2018-05-06 20:18:19 +0300

    added two

---
+++ sergey/src/main/java/TwoOne.java
@@ -0,0 +1,15 @@
+public class TwoOne {
+    public static void main(String[] args) {
+        String course =  "*  Course: Java core   *";
+        String student = "*       student        *";
+        String name =    "* Username: Swissgears *";
+        for (int i = 0; i<name.length();i++ ){
+               System.out.print("*");}
+        System.out.println();
+        System.out.println(course);
+        System.out.println(student);
+        System.out.println(name);
+        for (int i = 0; i<name.length();i++ ){
+            System.out.print("*");}
+    }
+}
