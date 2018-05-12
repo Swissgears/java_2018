81272ca919f81c81f849ce3eec755e4d5ebb2928
Swissgears <sergius.barannik@gmail.com>
2018-05-12 17:29:28 +0300

    Решение уравнений с разными типами примитивов

---
+++ sergey/src/main/java/HomeWork1_1.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class HomeWork1_1 {
+    public static void main(String[] args){
+        double x;
+        double y;
+        double z;
+        double n;
+        long f1;
+        int f2;
+        float f3;
+        double f4;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("введи число x");
+        x = scanner.nextDouble();
+        System.out.println("введи число y");
+        y = scanner.nextDouble();
+        System.out.println("введи число z");
+        z = scanner.nextDouble();
+        System.out.println("введи число n");
+        n = scanner.nextDouble();
+
+        f1 = (long)(x + (y - 100) * z / n);
+        System.out.println("f1 = x + (y - 100) * z / n)");
+        System.out.println("f1 = " + f1);
+        System.out.println("");
+
+        f2 = (int)((x - (50 + y * z) * n) + x);
+        System.out.println("f2 = (x - (50 + y * z) * n) + x");
+        System.out.println("f2 = " + f2);
+        System.out.println("");
+
+        f3 = (float) (x * (y / (z-1) + 555) - n);
+        System.out.println("f3 = x * (y / (z-1) + 555) - n");
+        System.out.println("f3 = " + f3);
+        System.out.println("");
+
+        f4 = -x / y + z * (n+1);
+        System.out.println("f4 = -x / y + z * (n+1)");
+        System.out.println("f4 = " + f4);
+        System.out.println("");
+
+    }
+}
